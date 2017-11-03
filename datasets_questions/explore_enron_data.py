61e8d678f89f713c5d91bb9dca43cc10ac7b777d
Rahul <rponnada91@gmail.com>
2017-11-03 11:20:21 -0500

    Enron data exploration

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 """ 
     Starter code for exploring the Enron dataset (emails + finances);
@@ -18,5 +19,29 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+#for name, feature in enron_data.items():
+#   print(name,feature)
 
 
+# 13. qz - # persons in data set
+# print len(enron_data)
+
+# 14. qz - each person how many features
+# print len(enron_data.items()[1][1])
+# for name, feature in enron_data.items():
+#   print(name,len(feature))
+
+# 15. qz - Finding POIs In The Enron Data
+# print (len({name:item['poi'] for name,item in enron_data.items() if item['poi']==1}))
+
+# 16.qz - How Many POIs Exist?
+# count #names from ../final_project/poi_email_addresses.py
+
+# 18. qz - What is the total value of the stock belonging to James Prentice?
+# print enron_data['PRENTICE JAMES']['total_stock_value']
+
+# 19. qz - How many email messages do we have from Wesley Colwell to persons of interest?
+# print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+# 20. qz - What’s the value of stock options exercised by Jeffrey K Skilling?
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
